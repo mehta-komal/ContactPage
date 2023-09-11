ee770babb154bb3afc6554c522386885e927842d
mehta-komal <komalmehta0111@gmail.com>
2023-09-11 14:53:36 +0530

    files deleted

--- pizza-hut/pizza-app/New folder/src/Config.js
+++
@@ -1,7 +0,0 @@
-const Config = {
-    apikeycart: process.env.REACT_APP_CART,
-    apikeydata: process.env.REACT_APP_DATA,
-    apikeyuserdata: process.env.REACT_APP_USERDATA,
-    apikeyorder:process.env.REACT_APP_ORDER
-  };
-export default Config  
