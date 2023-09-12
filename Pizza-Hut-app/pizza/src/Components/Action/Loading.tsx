9e604ed34e6a954bf34eb8d07a2edaff921c8bd4
mehta-komal <komalmehta0111@gmail.com>
2023-09-12 11:23:05 +0530

    update loading.tsx

--- Pizza-Hut-app/pizza/src/Components/Action/Loading.tsx
+++ Pizza-Hut-app/pizza/src/Components/Action/Loading.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Loading: React.FC = () => {
   return (
     <div className="loading">
-      <div className="loading-spinner"></div>
+      <div className="loading-spinner">
+        <h1>Loading....</h1>
+      </div>
     </div>
   );
 };
